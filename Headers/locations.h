cd6bdff0337686524ef42d3af06c2d8ecd34f25e
hostomani <hostomani@gmail.com>
2024-01-09 13:43:57 +0000

    LocationSDK initial publishing

---
+++ Headers/locations.h
@@ -0,0 +1,18 @@
+//
+//  locations.h
+//  locations
+//
+//  Created by Hosam Hasan on 1/6/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for locations.
+FOUNDATION_EXPORT double locationsVersionNumber;
+
+//! Project version string for locations.
+FOUNDATION_EXPORT const unsigned char locationsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <locations/PublicHeader.h>
+
+
